b41d1d2d245896af52f8794aad171dce3148b8b1
Thiago Silva Soares <tsoares.dev@gmail.com>
2023-01-20 10:17:52 -0300

    Update atv3

--- script.js
+++ script.js
@@ -30,20 +30,20 @@
 // N = 3, sua altura será de 6 metros(dobrou a altura na primavera seguinte)
 // Escreva a função alturaArvoreUtopica, que utilize uma quantidade de ciclos de crescimento, e retorne a altura resultante da árvore da Soulcode.
 
-function alturaArvoreUtopica(ciclos) {
-  var tArvore = 1;
-  for(var i = 0; i < ciclos; i++)
-    // if(i%2==1)
-    if(i & 1)
-      tArvore += 1;
-    else
-      tArvore *= 2;
-  return tArvore;
- }
- alert("Com N = 0. A arvore não cresceu nada, permace com " + alturaArvoreUtopica(0) + "M");
- alert("Com N = 1. A arvore dobrou a altura na primavera com " + alturaArvoreUtopica(1) + "M");
- alert("Com N = 2. A arvore cresceu um metro a mais no verão com " + alturaArvoreUtopica(2) + "M");
- alert("Com N = 3. A arvore dobrou a altura na primavera seguinte com " + alturaArvoreUtopica(3) + "M");
+// function alturaArvoreUtopica(ciclos) {
+//   var tArvore = 1;
+//   for(var i = 0; i < ciclos; i++)
+//     // if(i%2==1)
+//     if(i & 1)
+//       tArvore += 1;
+//     else
+//       tArvore *= 2;
+//   return tArvore;
+//  }
+//  alert("Com N = 0. A arvore não cresceu nada, permace com " + alturaArvoreUtopica(0) + "M");
+//  alert("Com N = 1. A arvore dobrou a altura na primavera com " + alturaArvoreUtopica(1) + "M");
+//  alert("Com N = 2. A arvore cresceu um metro a mais no verão com " + alturaArvoreUtopica(2) + "M");
+//  alert("Com N = 3. A arvore dobrou a altura na primavera seguinte com " + alturaArvoreUtopica(3) + "M");
 
 
 
@@ -55,8 +55,29 @@ function alturaArvoreUtopica(ciclos) {
 
 // Por exemplo, maisMenos([1, 2, 0, -1]) deveria retornar[0.5, 0.25, 0.25], devido a que há 50 % de positivos, 25 % de zeros, e 25 % de negativos.
 
-
-
+// alert("teste")
+var resultado = [];
+function maisMenos(arrayEntrada){
+	let nPositivo = 0;
+	let nZero = 0;
+	let nNegativo = 0;
+	
+	for(let i = 0; i < arrayEntrada.length; i++){
+		if(arrayEntrada[i] < 0)
+			nNegativo++;
+			
+		else if(arrayEntrada[i] == 0)
+			nZero++;
+			
+		else
+			nPositivo++;
+	}
+	resultado[0] = nPositivo / arrayEntrada.length;
+	resultado[1] = nZero / arrayEntrada.length;
+	resultado[2] = nNegativo / arrayEntrada.length;
+	return resultado;
+}
+alert(maisMenos([1, 2, 0, -1]));
 
 
 // ############################## Desafio 04: Produto ########################
